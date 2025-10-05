56d4d2c97d4813df7f9ff38e41a7bebead82e5e0
Ismail <Ismail@gmail.com>
2025-10-05 21:24:29 +0200

    added a name fiteration program by using a descripe tool and make much more awesome

---
+++ __test__/fiteration.test.js
@@ -0,0 +1,21 @@
+const nameFilter = require("../fiteration");
+
+describe("Validate The Input Field", () => {
+    test("Check If Name Is Empty", () => {
+        expect(nameFilter()).toBe("Unknown");
+    })
+
+    test("Check For Spaces (Start + End)", () => {
+        expect(nameFilter("      Ismail  ")).toBe("Ismail");
+    })
+
+    test("Check If Name.length > 10", () => {
+        expect(nameFilter("Ismail_Mohammed_Ahmed")).toBe("Ismail_Moh")
+    })
+    
+    test("Check If Name Not Starts with Underscore > 10", () => {
+        expect(nameFilter("_Ismail")).toBe("Ismail")
+    })
+
+
+})
